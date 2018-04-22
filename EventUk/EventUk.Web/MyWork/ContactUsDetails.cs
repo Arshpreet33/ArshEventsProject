0e1a76a74b35ebcde4bac388b248179984988964
arsh1184 <arsh1184@outlook.com>
2018-04-22 14:31:02 +0530

    Added Error Log file for getting error in text File

--- EventUk/EventUk.Web/MyWork/ContactUsDetails.cs
+++ EventUk/EventUk.Web/MyWork/ContactUsDetails.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
+using System.IO;
+using System.Web;
 
 namespace EventUk.Web.MyWork
 {
@@ -74,10 +76,37 @@ namespace EventUk.Web.MyWork
             }
             catch (Exception e)
             {
-                    return "Oops! Error Occured";
+                this.LogError(e);
+                return "Oops! Error Occured";
             }
 
             return "Success";
+        }
+
+        private void LogError(Exception ex)
+        {
+
+            string message = string.Format("Time: {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt"));
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Environment.NewLine;
+            message += string.Format("Message: {0}", ex.Message);
+            message += Environment.NewLine;
+            message += string.Format("StackTrace: {0}", ex.StackTrace);
+            message += Environment.NewLine;
+            message += string.Format("Source: {0}", ex.Source);
+            message += Environment.NewLine;
+            message += string.Format("TargetSite: {0}", ex.TargetSite.ToString());
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Environment.NewLine;
+            string path = HttpContext.Current.Server.MapPath("~/Logs/Logs.txt");
+            
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(message);
+                writer.Close();
+            }
         }
     }
 }
